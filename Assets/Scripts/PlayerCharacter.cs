18a24779fea6dceb389a0226f588b609978c95c0
ImIntegrity <106005889+Sophiecody2@users.noreply.github.com>
2022-09-30 22:46:37 -0400

    Update 6- Player Movement

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -7,6 +7,41 @@ public class PlayerCharacter : MonoBehaviour
     public int level = 1;
     public int health = 100;
 
+    public float pSpeed;
+    
+    private float upDown;
+    private float leftRight;
+
+    private bool facingRight = true;
+
+    public Rigidbody2D rb;
+
+    private void Update()
+    {
+        upDown = Input.GetAxisRaw("Vertical");
+        leftRight = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = new Vector2(leftRight * pSpeed, upDown * pSpeed);
+
+        if (leftRight > 0 && !facingRight)
+        {
+            Flip();
+        }
+        if (leftRight < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+
+        facingRight = !facingRight;
+    }
+
     public void SavePlayer ()
     {
         SaveSystem.SavePlayer(this);
